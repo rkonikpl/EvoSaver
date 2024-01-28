94a2e6fdac9ddbc3ea549ea1cee56c1a8cfeb409
Rafał Kobyliński <rkonik@rkonik.com>
2024-01-29 00:46:19 +0100

    Adding basic template files

---
+++ component.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @copyright   (C) 2024 rKonik Rafał Kobyliński <https://rkonik.com>
+ * @license     https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License, version 3 or later
+ */
+
+defined('_JEXEC') or die;
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @copyright   (C) 2024 rKonik Rafał Kobyliński <https://rkonik.com>
+ * @license     https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License, version 3 or later
+ */
+
+defined('_JEXEC') or die;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>EvoSaver</title>
+	</head>
+	<body>
+
+	</body>
+</html>
